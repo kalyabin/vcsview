f1302907642ee0e3b4ff9d05114f84f518d20699
Max Kalyabin <kalyabin@internet-design.ru>
2019-02-20 11:20:26 +0300

    refactor check testing repo

--- main_test.go
+++ main_test.go
@@ -12,33 +12,52 @@ const (
 	hgRepositoryPath = "testdata"+string(os.PathSeparator)+"hg"
 )
 
-// Check git testing repository
-// panic if repository not found
-func checkGitTestingRepo() error {
+// Check testing repository
+// repoType - string identifier of repository type (hg, git)
+// repoPath - path of repository (not a .git or .hg path)
+// returns error if directory or repository not found or not initialized
+func checkRepo(repoType string, repoPath string) error {
+	repoTypeName := ""
+	vcsPath := repoPath
+	cmdName := ""
+
+	switch repoType {
+	case "hg":
+		repoTypeName = "Mercurial"
+		vcsPath += string(os.PathSeparator) + ".hg"
+		cmdName = "hg"
+	case "git":
+		repoTypeName = "GIT"
+		vcsPath += string(os.PathSeparator) + ".git"
+		cmdName = "git"
+	}
+
 	notFoundErr := func(path string) error {
-		return fmt.Errorf("Git repository path not found: %s\nPlease, install repository using test_install.sh script.\n", path)
+		return fmt.Errorf("%s repository path not found: %s\nPlease, install repository using test_install.sh script.\n", repoTypeName, path)
 	}
 
-	if stat, err := os.Stat(gitRepositoryPath); err != nil || !stat.IsDir() {
-		return notFoundErr(gitRepositoryPath)
+	if stat, err := os.Stat(repoPath); err != nil || !stat.IsDir() {
+		return notFoundErr(repoPath)
 	}
 
-	gitPath := gitRepositoryPath+string(os.PathSeparator)+".git"
-	if stat, err := os.Stat(gitPath); err != nil || !stat.IsDir() {
-		return notFoundErr(gitPath)
+	if stat, err := os.Stat(vcsPath); err != nil || !stat.IsDir() {
+		return notFoundErr(vcsPath)
 	}
 
-	cmd := exec.Command("git", "status")
-	cmd.Dir = gitRepositoryPath
+	cmd := exec.Command(cmdName, "status")
+	cmd.Dir = repoPath
 	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("Git repository not found at %s: %v.\nPlease, re-install repository using test_install.sh script.\n", gitRepositoryPath, err)
+		return fmt.Errorf("%s repository not found at %s: %v.\nPlease, re-install repository using test_install.sh script.\n", repoTypeName, repoPath, err)
 	}
 
 	return nil
 }
 
 func TestMain(m *testing.M) {
-	if err := checkGitTestingRepo(); err != nil {
+	if err := checkRepo("git", gitRepositoryPath); err != nil {
+		panic(err)
+	}
+	if err := checkRepo("hg", hgRepositoryPath); err != nil {
 		panic(err)
 	}
 	code := m.Run()
