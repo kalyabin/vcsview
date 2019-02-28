68788b6da1de074f3ca6b410f64e745d8764b9ed
Max Kalyabin <kalyabin@internet-design.ru>
2019-02-28 16:55:20 +0300

    fix tests

--- git_test.go
+++ git_test.go
@@ -392,6 +392,11 @@ func TestGit_ReadHistoryFail(t *testing.T) {
 func TestGit_ReadHistoryOk(t *testing.T) {
 	g := MakeGitMock(t)
 
+	branchSplit := func(branch string) string {
+		bPieces := strings.Split(branch, "/")
+		return bPieces[len(bPieces)-1]
+	}
+
 	cases := []struct{
 		repoPath string
 		path string
@@ -402,10 +407,10 @@ func TestGit_ReadHistoryOk(t *testing.T) {
 		{gitRepositoryPath, "", "", 0, 10},
 		{gitRepositoryPath, "testpath", "", 0, 1},
 		{gitRepositoryPath, "testpath", "", 0, 1},
-		{gitRepositoryPath, "", expectedGitBranches[0], 0, 1},
-		{gitRepositoryPath, "", expectedGitBranches[1], 0, 1},
-		{gitRepositoryPath, "", expectedGitBranches[2], 0, 1},
-		{gitRepositoryPath, "", expectedGitBranches[3], 0, 1},
+		{gitRepositoryPath, "", branchSplit(expectedGitBranches[0]), 0, 1},
+		{gitRepositoryPath, "", branchSplit(expectedGitBranches[1]), 0, 1},
+		{gitRepositoryPath, "", branchSplit(expectedGitBranches[2]), 0, 1},
+		{gitRepositoryPath, "", branchSplit(expectedGitBranches[3]), 0, 1},
 		{gitRepositoryPath, "", "", 2, 2},
 	}
 
